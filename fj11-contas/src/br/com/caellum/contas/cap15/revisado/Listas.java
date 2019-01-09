c86ded4e7bf6c85cbdc7e3460dc4e696b9ce83f7
Andrey <almeidao94@gmail.com>
2019-01-09 17:51:58 -0200

    metódos list

--- fj11-contas/src/br/com/caellum/contas/cap15/revisado/Listas.java
+++ fj11-contas/src/br/com/caellum/contas/cap15/revisado/Listas.java
@@ -3,6 +3,8 @@ package br.com.caellum.contas.cap15.revisado;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
 import br.com.caelum.contas.modelo.Conta;
 import br.com.caelum.contas.modelo.ContaCorrente;
 
@@ -29,20 +31,65 @@ public class Listas {
 		Conta conta1 = new ContaCorrente();
 		Conta conta2 = new ContaCorrente();
 
-		contas.add(1, conta1);
+		contas.add(1, conta1); // add()
 		contas.add(2, conta2);
-		conta2.deposita(200);
+		conta2.deposita(200); // addAll()
 
 		contas.addAll(listaNomes);
 
 		for (int i = 0; i < contas.size(); i++) {
-			System.out.println(" " + i + " - " + contas.get(i));
+			System.out.println(" " + i + " - " + contas.get(i)); // get()
 		}
 
-		System.out.println("Contas na lista: " + contas.size());
+		System.out.println("Contas na lista: " + contas.size()); // size()
+
+		System.out.println("Contém conteúdo? " + contas.contains("Samantha")); // contains()
+		System.out.println("Contém conteúdo? " + contas.containsAll(listaNomes)); // containsAll()
+
+		System.out.println("Contém x elemento na lista? " + contas.equals(contas)); // equals()
+
+		try {
+			System.out.println(contas.get(5));
+		} catch (Exception e) {
+			System.out.println("Valor do índice está vázio");
+		}
+
+		System.out.println("HashCode da lista: " + contas.hashCode()); // hashcode()
+		System.out.println("HashCode da lista: " + listaNomes.hashCode());
+
+		// indexOf()
+		if (contas.indexOf(listaNomes) == 0) {
+			System.out.println("Contém na Lista");
+		} else if (contas.indexOf(listaNomes) == -1) {
+			System.out.println("Não contém na Lista");
+		}
+
+		System.out.println("Contém na Lista? " + contas.isEmpty()); // isEmpty()
+
+		System.out.println("Interator: " + contas.iterator()); // interator()
+
+		if (contas.lastIndexOf(1) != -1) {
+			System.out.println(contas.lastIndexOf(listaNomes));
+		} else {
+			System.out.println("Não contém na lista");
+		}
+
+		System.out.println(contas.listIterator());
+
+		// contas.remove(1);
+		// contas.removeAll(listaNomes);
+
+		// for (int i = 0; i < contas.size(); i++) {
+		// System.out.println(" " + i + " - " + contas.get(i)); // get()
+		// }
+
+		contas.set(2, conta); //set(index, element)
+		for (int i = 0; i < contas.size(); i++) {
+			System.out.println(" " + i + " - " + contas.get(i)); // get()
+		}
 		
-		System.out.println("Contém conteúdo? "+contas.contains(listaNomes)); 
-		
+//		contas.sort(c);
+
 	}
 
 }
